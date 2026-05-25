c94b4bca73294bacd7c89d69f14b4886dc1e516b
xande <xande@XanderCPU>
2026-05-24 20:25:43 -0500

    My 1st commit, simple main file

---
+++ My_Video_Game/src/main_files/Main.java
@@ -0,0 +1,18 @@
+package main_files;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.*;
+import java.io.IOException;
+
+public class Main {
+	
+	public static void main(String[] args)
+	{
+		JFrame frame = new JFrame("Maid Girl Mary");
+	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    frame.setSize(400, 300);
+	    frame.setLocationRelativeTo(null);
+	    frame.setVisible(true);
+	}
+}
